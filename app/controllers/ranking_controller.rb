4134a4b2407eee1bbecbac5f09ea43f3c459dffd
kazuma <kazuma-narita@united.jp>
2016-04-25 10:01:16 +0000

    ranking

---
+++ app/controllers/ranking_controller.rb
@@ -0,0 +1,12 @@
+class RankingController < ApplicationController
+    
+    before_action :logged_in_user
+    
+    def show
+        if params[:id] == "want"
+            @items = Want.limit(10).group(:item_id).order('count_all desc').count
+        elsif params[:id] == "have"
+            @items = Have.limit(10).group(:item_id).order('count_all desc').count
+        end
+    end
+end
